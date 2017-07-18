ae7fcbfe68fb1d74fc938dbb1b0139910e35ca40
easesstone <576395455@qq.com>
2017-07-18 13:52:25 +0800

    1,完成HBase 数据同步到Elasticsearch 模块的代码编写； 2，修改parent pom 文件，给maven-compile 插件添加utf-8 编码； 3，单独添加一个模块来完成 HBase 到Es 数据的同步。 (æ³ï备注：改变的HBaseHelper 的位置ï，其不需要添加到协处理器中） â

---
+++ HBase2Es/src/main/java/com/hzgc/hbase2es/HBase2EsObserver.java
@@ -0,0 +1,233 @@
+package com.hzgc.hbase2es;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.CoprocessorEnvironment;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Durability;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
+import org.apache.hadoop.hbase.coprocessor.ObserverContext;
+import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
+import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.log4j.Logger;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequestBuilder;
+import org.elasticsearch.action.support.WriteRequest;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateRequestBuilder;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class HBase2EsObserver  extends BaseRegionObserver{
+
+    private static Logger logger = Logger.getLogger(BaseRegionObserver.class);
+
+    //  从外部获取Es 集群的信息
+    private static void readConfiguration(CoprocessorEnvironment env){
+        Configuration conf = env.getConfiguration();  // 集群信息
+        EsClientUtils.clusterName = conf.get("es_cluster");  // es集群名字
+        EsClientUtils.nodeHosts = conf.get("es_hosts");    // es 列表,传入字符串，用逗号分隔
+        EsClientUtils.nodePort = conf.getInt("es_port", 9300);  // es的端口
+        EsClientUtils.indexName = conf.get("es_index");  // 索引
+        EsClientUtils.typeName = conf.get("es_type");       // 类型
+
+
+        logger.info("======================================");
+        logger.info("the es cluster info :===================== cluser_name " + EsClientUtils.clusterName
+                +  ", cluster_hosts: , " + EsClientUtils.nodeHosts
+                + ", cluster_port: " + EsClientUtils.nodePort + ",index: " + EsClientUtils.indexName +
+                ",type: " + EsClientUtils.typeName  + "===========================");
+        logger.info("=======================================");
+    }
+
+    @Override
+    public void start(CoprocessorEnvironment env) throws IOException {
+        readConfiguration(env);       // 读取Es 配置信息
+        EsClientUtils.initEsClient();  // 初始化Es 客户端
+    }
+
+    @Override
+    public void stop(CoprocessorEnvironment e) throws IOException {
+        EsClientUtils.clostEsClient();
+    }
+
+    @Override
+    public void postPut(ObserverContext<RegionCoprocessorEnvironment> e, Put put, WALEdit edit, Durability durability) {
+        logger.info("============something triggred when afere put================");
+        String indexId = new String(put.getRow());   // 通过Put 对象获取rowkey 作为id，
+        NavigableMap<byte[], List<Cell>> familyMap = put.getFamilyCellMap();
+        Map<String, Object> infoJson = new HashMap<String, Object>();
+        for (Map.Entry<byte[], List<Cell>> entry : familyMap.entrySet()) {
+            for (Cell cell : entry.getValue()) {
+                String key = Bytes.toString(CellUtil.cloneQualifier(cell));
+                String value = Bytes.toString(CellUtil.cloneValue(cell));
+                logger.info("===================属性====================");
+                logger.info("=============key: " + key + "  value: " + value);
+                if (!"photo".equals(key) && !"reason".equals(key)){
+                    infoJson.put(key, value);
+                } else {
+                    logger.info("++++++++++++++++ Skip someunusethines +++++++++++");
+                }
+            }
+        }
+        ElasticSearchBulkOperator.addUpdateBuilderToBulk(EsClientUtils.client.prepareUpdate(EsClientUtils.indexName,
+                EsClientUtils.typeName, indexId).setDocAsUpsert(true).setDoc(infoJson));
+    }
+
+    @Override
+    public void postDelete(ObserverContext<RegionCoprocessorEnvironment> e,
+                           Delete delete, WALEdit edit, Durability durability) {
+        logger.info("=======================================================");
+        logger.info("===============something triggred when afere DELETE===========");
+        String indexId = new String(delete.getRow());
+        logger.info("========inde: " + indexId + " ====================");
+        try {
+            ElasticSearchBulkOperator.addDeleteBuilderToBulk(
+                    EsClientUtils.client.prepareDelete(EsClientUtils.indexName,
+                            EsClientUtils.typeName, indexId));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+class EsClientUtils{
+    public static String clusterName;
+    public static String nodeHosts;
+    public static int nodePort;
+    public static String indexName;
+    public static String typeName;
+    public static Client client;
+
+    public static void initEsClient() throws UnknownHostException {
+        Settings settings = Settings.builder()
+                .put("cluster.name", EsClientUtils.clusterName).build();
+        System.out.println("++++++++++++++++++++++"  + EsClientUtils.nodeHosts);
+        for (String host:EsClientUtils.nodeHosts.split(",")){
+            client = new PreBuiltTransportClient(settings)
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host),
+                            EsClientUtils.nodePort));
+        }
+    }
+
+    public static void clostEsClient(){
+        EsClientUtils.client.close();
+    }
+}
+
+
+class ElasticSearchBulkOperator {
+    private static final int MAX_BULK_COUNT = 10000;
+    private static BulkRequestBuilder bulkRequestBuilder = null;
+
+    private static final Lock commitLock = new ReentrantLock();
+
+    private static ScheduledExecutorService scheduledExecutorService = null;
+
+    static {
+        // init es bulkRequestBuilder
+        bulkRequestBuilder = EsClientUtils.client.prepareBulk();
+        bulkRequestBuilder.setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE);
+
+        // init thread pool and set size 1
+        scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+        // create beeper thread( it will be sync data to ES cluster)
+        // use a commitLock to protected bulk es as thread-save
+        final Runnable beeper = new Runnable() {
+            public void run() {
+                commitLock.lock();
+                try {
+                    bulkRequest(0);
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                } finally {
+                    commitLock.unlock();
+                }
+            }
+        };
+
+        // set time bulk task
+        // set beeper thread(10 second to delay first execution , 30 second period between successive executions)
+        scheduledExecutorService.scheduleAtFixedRate(beeper, 10, 30, TimeUnit.SECONDS);
+
+    }
+
+    /**
+     * shutdown time task immediately
+     */
+    public static void shutdownScheduEx() {
+        if (null != scheduledExecutorService && !scheduledExecutorService.isShutdown()) {
+            scheduledExecutorService.shutdown();
+        }
+    }
+
+    /**
+     * bulk request when number of builders is grate then threshold
+     *
+     * @param threshold
+     */
+    private static void bulkRequest(int threshold) {
+        if (bulkRequestBuilder.numberOfActions() > threshold) {
+            BulkResponse bulkItemResponse = bulkRequestBuilder.execute().actionGet();
+            if (!bulkItemResponse.hasFailures()) {
+                bulkRequestBuilder = EsClientUtils.client.prepareBulk();
+            }
+        }
+    }
+
+    /**
+     * add update builder to bulk
+     * use commitLock to protected bulk as thread-save
+     * @param builder
+     */
+    public static void addUpdateBuilderToBulk(UpdateRequestBuilder builder) {
+        commitLock.lock();
+        try {
+            bulkRequestBuilder.add(builder);
+            bulkRequest(MAX_BULK_COUNT);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            commitLock.unlock();
+        }
+    }
+
+    /**
+     * add delete builder to bulk
+     * use commitLock to protected bulk as thread-save
+     *
+     * @param builder
+     */
+    public static void addDeleteBuilderToBulk(DeleteRequestBuilder builder) {
+        commitLock.lock();
+        try {
+            bulkRequestBuilder.add(builder);
+            bulkRequest(MAX_BULK_COUNT);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            commitLock.unlock();
+        }
+    }
+}
---
+++ HBase2Es/src/test/java/com/hzgc/hbase2es/HBase2EsSuite.java
@@ -0,0 +1,49 @@
+package com.hzgc.hbase2es;
+
+import com.hzgc.hbase2es.utils.HBaseHelper;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.client.Table;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HBase2EsSuite {
+
+    @Test
+    public void test(){
+//        String hosts = "s100,s200,s300";
+//        String[] hostlist = hosts.split(",");
+//        for (String host:hostlist){
+//            System.out.println(host);
+//        }
+        System.out.println(new Path("hdfs:///user/ldl/hbase2es-2.0-jar-with-dependencies.jar"));
+        System.out.println((char) 47);
+    }
+
+    @Test
+    public void testCrateTableWithCoprocessor(){
+        //es_cluster=zcits,es_type=zcestestrecord,es_index=zcestestrecord,es_port=9100,es_host=master
+        // 注意，索引的命名规则，必须是小写字母的，不可以包含大写字母
+        Map<String, String> mapOfOberserverArgs = new HashMap<String, String>();
+        mapOfOberserverArgs.put("es_cluster","my-cluser");
+        mapOfOberserverArgs.put("es_index","objectinfo");
+        mapOfOberserverArgs.put("es_type","pcl");
+        mapOfOberserverArgs.put("es_hosts","s200");
+        HBaseHelper helper = new HBaseHelper();
+        try {
+            helper.dropTable("ObjectInfo");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Table table = helper.crateTableWithCoprocessor("ObjectInfo", "com.hzgc.hbase2es.HBase2EsObserver",
+                "hdfs:///user/ldl/hbase2es-1.0-jar-with-dependencies.jar", mapOfOberserverArgs, 3, "pcl");
+        try {
+            HBaseHelper.getHBaseConnection().close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(table);
+    }
+}
