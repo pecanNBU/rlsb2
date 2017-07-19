be48a01f9d4f5eb89166f084946f1e7ae0cd087f
gosunliu <lsb@hzgosun.com>
2017-07-19 14:06:04 +0800

    获取动态人脸照片（接口）（外）
    修改对象属性
    添加get set方法

--- Dubbo/src/main/java/com/hzgc/dubbo/calculate/PersonPhoto.java
+++ Dubbo/src/main/java/com/hzgc/dubbo/calculate/PersonPhoto.java
@@ -5,42 +5,81 @@ public class PersonPhoto {
     /**
      * 动态人脸照片ID
      */
-    protected String imageId;
+    private String imageId;
 
     /**
      * 抓取照片的设备ID
      */
-    protected String ipc;
-
-
+    private String ipc;
     /**
      * 人脸大图数据
      */
-    protected byte[] bigImage;
+    private byte[] bigImage;
 
     /**
      * 人脸小图数据
      */
-    protected byte[] smallImage;
+    private byte[] smallImage;
 
     /**
      * 图片描述信息
      */
-    protected String des;
+    private String des;
 
     /**
      * 图片附加信息
      */
-    protected String ex;
+    private String ex;
 
-    /**
-     * 属性1
-     */
-    protected String property1;
-    /**
-     * 属性n
-     */
-    protected String propertyn;
+
+    public String getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(String imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getIpc() {
+        return ipc;
+    }
+
+    public void setIpc(String ipc) {
+        this.ipc = ipc;
+    }
+
+    public byte[] getBigImage() {
+        return bigImage;
+    }
+
+    public void setBigImage(byte[] bigImage) {
+        this.bigImage = bigImage;
+    }
+
+    public byte[] getSmallImage() {
+        return smallImage;
+    }
+
+    public void setSmallImage(byte[] smallImage) {
+        this.smallImage = smallImage;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public String getEx() {
+        return ex;
+    }
+
+    public void setEx(String ex) {
+        this.ex = ex;
+    }
+
 
 
 
