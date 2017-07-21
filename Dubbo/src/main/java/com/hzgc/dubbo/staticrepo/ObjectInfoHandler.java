c76e99730fa38711040c1a01ac02f98e41679135
easesstone <576395455@qq.com>
2017-07-21 11:05:37 +0800

    1,修改dubbo 模块pom文件，用来生成javadocs。.

--- Dubbo/src/main/java/com/hzgc/dubbo/staticrepo/ObjectInfoHandler.java
+++ Dubbo/src/main/java/com/hzgc/dubbo/staticrepo/ObjectInfoHandler.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public interface ObjectInfoHandler {
 
     /**
-     * 针对单个对象信息的添加处理  （外） （李第亮）
+     * 针对单个对象信息的添加处理  （外）（李第亮）
      * @param  platformId 表示的是平台的ID， 平台的唯一标识。
      * @param person K-V 对，里面存放的是字段和值之间的一一对应关系,
      *               例如：传入一个Map 里面的值如下map.put("idcard", "450722199502196939")
@@ -17,21 +17,21 @@ public interface ObjectInfoHandler {
     public byte addObjectInfo(String platformId, Map<String, Object> person);
 
     /**
-     * 删除对象的信息  （外）  （李第亮）
+     * 删除对象的信息  （外）（李第亮）
      * @param rowkeys 具体的一个人员信息的ID，值唯一
      * @return 返回值为0，表示删除成功，返回值为1，表示删除失败
      */
     public int deleteObjectInfo(List<String> rowkeys);
 
     /**
-     * 修改对象的信息   （外）  （李第亮）
+     * 修改对象的信息   （外）（李第亮）
      * @param person K-V 对，里面存放的是字段和值之间的一一对应关系，参考添加里的描述
-     * @return返回值为0，表示更新成功，返回值为1，表示更新失败
+     * @return 返回值为0，表示更新成功，返回值为1，表示更新失败
      */
     public int updateObjectInfo(Map<String, Object> person);
 
     /**
-     * 可以匹配精确查找，以图搜索人员信息，模糊查找   （外）  （李第亮）
+     * 可以匹配精确查找，以图搜索人员信息，模糊查找   （外）（李第亮）
      * @param platformId 对应的是平台的ID
      * @param idCard 身份证号
      * @param rowkey 对应的是一个人在HBase 数据库中的唯一标志
@@ -42,17 +42,13 @@ public interface ObjectInfoHandler {
      * @param start 需要返回的起始行
      * @param pageSize 需要返回的每页的大小
      * @param serachId 搜索Id
-     * @param serachType 搜索的类型，有如下搜索类型：
-     * searchByPlatFormIdAndIdCard（身份证号），
-     * searchByRowkey（rowke），
-     * serachByCphone(布控人手机号)
-     * searchByCreator（布控人），
-     * searchByName（多条件查询），
-     * searchByMuti（多条件&& 查询）
+     * @param serachType 搜索的类型，有如下搜索类型,searchByPlatFormIdAndIdCard（身份证号），
+     *                   searchByRowkey（rowke），
+     *                   serachByCphone(布控人手机号)
+     *                   searchByCreator（布控人）
+     *                   searchByMuti（多条件查询）
      * @param moHuSearch  是否模糊查询
-     * @return 返回一个ObjectSearchResult 对象，
-     * 里面包含了本次查询ID，查询成功标识，
-     * 查询照片ID（无照片，此参数为空），结果数，人员信息列表
+     * @return 返回搜索所需要的结果封装成的对象，包含搜索id，成功与否标志，记录数，记录信息，照片id
      */
     public ObjectSearchResult getObjectInfo(String platformId, String idCard,
                                             String rowkey, byte[] image,
@@ -63,45 +59,41 @@ public interface ObjectInfoHandler {
                                             boolean moHuSearch);
 
     /**
-     * 根据传进来的平台id  和身份证号进行查询  （外） （李第亮）
+     * 根据传进来的平台id  和身份证号进行查询  （外）（李第亮）
      * @param platformId  平台ID
      * @param IdCard  身份证号
      * @param moHuSearch  是否模糊查询
      * @param start  返回的查询记录中，从哪一条开始
      * @param pageSize  需要返回的记录数
-     * @return 返回一个ObjectSearchResult 对象，里面包含了本次查询ID，
-     * 查询成功标识，查询照片ID（无照片，此参数为空），结果数，人员信息列表
+     * @return 返回搜索所需要的结果封装成的对象，包含搜索id，成功与否标志，记录数，记录信息，照片id
      */
     public ObjectSearchResult searchByPlatFormIdAndIdCard(String platformId,
                                                           String IdCard, boolean moHuSearch,
                                                           long start, long pageSize);
 
     /**
-     * 根据rowkey 进行查询 （外） （李第亮）
+     * 根据rowkey 进行查询 （外）
      * @param rowkey  标记一条对象信息的唯一标志。
-     * @return  返回一个ObjectSearchResult 对象，里面包含了本次查询ID，
-     * 查询成功标识，查询照片ID（无照片，此参数为空），结果数，人员信息列表
+     * @return  返回搜索所需要的结果封装成的对象，包含搜索id，成功与否标志，记录数，记录信息，照片id
      */
     public ObjectSearchResult searchByRowkey(String rowkey);
 
 
     /**
-     * 根据布控人手机号进行查询  （外） （李第亮）
+     * 根据布控人手机号进行查询  （外）
      * @param cphone 布控人手机号
-     * @return 返回一个ObjectSearchResult 对象，里面包含了本次查询ID，
-     * 查询成功标识，查询照片ID（无照片，此参数为空），结果数，人员信息列表
+     * @return 返回搜索所需要的结果封装成的对象，包含搜索id，成功与否标志，记录数，记录信息，照片id
      */
     public ObjectSearchResult serachByCphone(String cphone);
 
 
     /**
-     * 根据布控人姓名进行查询  （外） （李第亮）
+     * 根据布控人姓名进行查询  （外）
      * @param creator  布控人姓名
      * @param moHuSearch  是否模糊查询
      * @param start  返回的查询记录中，从哪一条开始
      * @param pageSize  需要返回的记录数
-     * @return  返回一个ObjectSearchResult 对象，
-     * 里面包含了本次查询ID，查询成功标识，查询照片ID（无照片，此参数为空），结果数，人员信息列表
+     * @return  返回搜索所需要的结果封装成的对象，包含搜索id，成功与否标志，记录数，记录信息，照片id
      */
     public ObjectSearchResult searchByCreator(String creator, boolean moHuSearch,
                                               long start, long pageSize);
@@ -112,8 +104,7 @@ public interface ObjectInfoHandler {
      * @param moHuSearch  是否模糊查询
      * @param start  返回的查询记录中，从哪一条开始
      * @param pageSize  需要返回的记录数
-     * @return  返回一个ObjectSearchResult 对象，里面包含了本次查询ID，
-     * 查询成功标识，查询照片ID（无照片，此参数为空），结果数，人员信息列表
+     * @return  返回搜索所需要的结果封装成的对象，包含搜索id，成功与否标志，记录数，记录信息，照片id
      */
     public ObjectSearchResult searchByName(String name, boolean moHuSearch,
                                            long start, long pageSize);
@@ -127,7 +118,7 @@ public interface ObjectInfoHandler {
      * @param feature 特征值
      * @param start  返回的查询记录中，从哪一条开始
      * @param pageSize  需要返回的记录数
-     * @return
+     * @return 返回搜索所需要的结果封装成的对象，包含搜索id，成功与否标志，记录数，记录信息，照片id
      */
     public ObjectSearchResult serachByPhotoAndThreshold(String platformId, byte[] photo,
                                                         int threshold, String feature,
