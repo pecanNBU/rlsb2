358b7340061809567f521bc148ead919229966b3
chenyonglong <cylsssb@126.com>
2017-07-22 16:29:06 +0800

    'RealTimeFaceCompare'

---
+++ Dubbo/src/main/java/com/hzgc/dubbo/ftpAddress/ftpAddressService.java
@@ -0,0 +1,10 @@
+package com.hzgc.dubbo.ftpAddress;
+
+public interface ftpAddressService {
+	/**
+	 * return the address of available ftp
+	 * @return ip:port String
+	 */
+	public String getftpAddress();
+	
+}
---
+++ Dubbo/src/main/java/com/hzgc/dubbo/ftpAddress/ftpAddressServiceImpl.java
@@ -0,0 +1,9 @@
+package com.hzgc.dubbo.ftpAddress;
+
+public class ftpAddressServiceImpl implements ftpAddressService {
+
+	public String getftpAddress() {
+		return "192.168.1.1" + ":21";
+	}
+
+}
