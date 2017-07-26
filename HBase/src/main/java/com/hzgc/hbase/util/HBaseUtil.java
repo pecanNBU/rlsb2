06725238cf25689a20efee0af620f6c309efb1fd
赵喆 <dgxdks@163.com>
2017-07-26 14:28:03 +0800

    设备管理和布控预案模块代码实现
    添加了设备的绑定删除和修改功能

--- HBase/src/main/java/com/hzgc/hbase/util/HBaseUtil.java
+++ HBase/src/main/java/com/hzgc/hbase/util/HBaseUtil.java
@@ -1,13 +1,32 @@
 package com.hzgc.hbase.util;
 
+import org.apache.hadoop.hbase.client.Table;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 public class HBaseUtil {
     private static Logger LOG = Logger.getLogger(HBaseUtil.class);
+
+    public static boolean strIsRight(String str) {
+        if (null != str && str.length() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void closTable(Table table) {
+        if (null != table) {
+            try {
+                table.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static File loadResourceFile(String resourceName) {
         if (false) {
