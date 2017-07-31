ac061fa1537a490c754c99daa66f74ae39ef6833
easesstone <576395455@qq.com>
2017-07-31 17:23:45 +0800

    1,添加一个table类，里面存放表名，列族名，列明等，方便后续版本改标的名字，以及方便和平台组交流维护。2，对应着table类，修改添加修改和删除的对应属性。3，修改更新接口，更新接口里面没有在更新的时候更新时间。

---
+++ Dubbo/src/main/java/com/hzgc/dubbo/staticrepo/ObjectInfoTable.java
@@ -0,0 +1,20 @@
+package com.hzgc.dubbo.staticrepo;
+
+public class ObjectInfoTable {
+    public static final String TABLE_NAME = "objectinfo"; // 表名
+    public static final String ROWKEY = "id";
+    public static final String PERSON_COLF = "person";  // person列族
+    public static final String PLATFORMID = "platformid";  // 平台ID
+    public static final String TAG = "tag";            // 人车标志
+    public static final String PKEY = "pkey";          // 人员类型
+    public static final String NAME = "name";         // 姓名
+    public static final String IDCARD = "idcard";    // 身份证号
+    public static final String SEX = "sex";           //  性别
+    public static final String PHOTO = "photo";         // 照片
+    public static final String FEATURE = "feature";  // 特征值
+    public static final String REASON = "reason";     // 理由
+    public static final String CTREATOR = "creator";    // 布控人
+    public static final String CPHONE = "cphone";      // 布控人手机号
+    public static final String CREATETIME = "createtime";     // 创建时间
+    public static final String UPDATETIME = "updatetime";     // 更新时间
+}
